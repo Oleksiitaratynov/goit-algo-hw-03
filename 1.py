d57768adca1434bb80cb997bcb57dbe7f0b4b82a
Oleksiitaratynov <alxt0125@gmail.com>
2024-03-11 13:53:47 +0100

    first commit

--- 1.py
+++ 1.py
@@ -1,14 +1,16 @@
-from datetime import datetime
+import datetime
 
-t = '11.03.2024'
+def get_days_from_today(date):
+    try:
+        input_date = datetime.datetime.strptime(date, '%Y-%m-%d')
+        current_date = datetime.datetime.today()
+        days_difference = (current_date - input_date).days
+        
+        return days_difference
+    except ValueError:
+        return "Неправильний формат дати. Введіть дату у форматі 'РРРР-ММ-ДД'."
 
-d = datetime.strptime(t, '%d.%m.%Y')
-print(d.date())
-print(d.day, d.month, d.year)
-print(type(d))
-
-
-other = d.replace(month=4, day=1, hour=14, minute=55)
-print(other)
-a = other.strftime('%Y/%m/%d')
-print(a)
+today = datetime.datetime.today().strftime('%Y-%m-%d')
+example_date = "2023-10-09"
+result = get_days_from_today(example_date)
+print(f"Різниця між {example_date} та {today}: {result} днів")
--- 2.py
+++ 2.py
@@ -1,12 +1,15 @@
-from datetime import timedelta, datetime
-delta = timedelta(
-    days=10,
-    seconds=27,
-    minutes=5,
-    hours=8,
-    weeks=2
-)
-now = datetime.now()
+import random
 
-result = now + delta
-print(result)
+def get_numbers_ticket(min_num, max_num, quantity):
+
+    if not (1 <= min_num <= max_num <= 1000):
+        return []
+    random_numbers = set()
+    while len(random_numbers) < quantity:
+        random_numbers.add(random.randint(min_num, max_num))
+
+    sorted_numbers = sorted(random_numbers)
+    return sorted_numbers
+
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
